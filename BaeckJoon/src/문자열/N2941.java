ad08b8bc86ff549cdd674fc5c23ee7b177dd9162
LastChallenged <LastChallenged@gmail.com>
2020-07-16 01:16:03 +0900

    2020-07-16 update N2941

---
+++ BaeckJoon/src/문자열/N2941.java
@@ -0,0 +1,36 @@
+//https://hongku.tistory.com/243
+
+package ¹®ÀÚ¿­;
+
+import java.io.*;
+
+public class N2941 {
+	
+	
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String s = bf.readLine();
+		
+		bf.close();
+		
+		String[] Croatia_Alphabet = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+		
+		for(int i = 0; i < Croatia_Alphabet.length; i++) {
+			//ljes=njak
+			if(s.contains(Croatia_Alphabet[i])) {
+				s = s.replaceAll(Croatia_Alphabet[i], "*");
+				
+			}
+		}
+		
+		int result = s.length();
+		
+		bw.write(result + "\n");
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
